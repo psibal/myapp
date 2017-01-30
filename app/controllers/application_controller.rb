c1f806b15d1876ff49fc6970d77b7c3b8022edd5
psibal <psibal@yahoo.com>
2017-01-30 17:05:05 -0500

    inital

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, world! from Hillsborough"
+    render html: "hello, world! from Hillsborough using github"
   end
 
 end
